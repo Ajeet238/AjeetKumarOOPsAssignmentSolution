996630922e64937ec98fc5547bd597b9e743fee3
ajeet <ajeetkumar23898@gmail.com>
2022-03-21 22:51:24 +0530

    commit

---
+++ gradedAssessment1Oops/src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import adminDepartment.AdminDepartment;
+import hrDepartment.HrDepartment;
+import techDepartment.TechDepartment;
+
+public class Main {
+	public static void main(String args[]) {
+		AdminDepartment admin = new AdminDepartment();
+		System.out.println(admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayHoliday());
+		System.out.println("");
+
+		HrDepartment hr = new HrDepartment();
+		System.out.println(hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayHoliday());
+		System.out.println("");
+
+		TechDepartment tech = new TechDepartment();
+		System.out.println(tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayHoliday());
+
+	}
+}
---
+++ gradedAssessment1Oops/src/superDepartment/SuperDepartment.java
@@ -0,0 +1,19 @@
+package superDepartment;
+
+public class SuperDepartment {
+	public String departmentName() {
+		return "Welcome to Super Department";
+	}
+	
+	public String getTodaysWork() {
+		return "No Work as of now";
+	}
+	
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+	
+	public String isTodayHoliday() {
+		return "Today is not a holiday";
+	}
+}
